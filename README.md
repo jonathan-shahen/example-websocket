72e3aa74cd8ff5ccff37881bb66df8e7be5df136
Jonathan Shahen <jonathan.shahen@gmail.com>
2022-03-16 22:19:10 -0700

    Init live cursors

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# example-websocket
+# Example Websocket Projects
+
+## Live Cursor Update
+
+A quick example of Node Express + `ws` Websocket library to show the (x,y) position of all cursors connected to the 
+server.
+
+### How to test
+
+![Live Cursors example](figs/live-cursors.png)
+
+1. Git clone this repo
+1. `cd live-cursors`
+1. `npm install`
+1. `npm start`
+1. Open Multiple browser tabs/windows to http://localhost:3000/
+1. Move the mouse on all of them to see the live updating
---
+++ live-cursors/src/index.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const uuid = require('uuid');
+const url = require('url');
+
+// Express Setup
+const express = require('express')
+const app = express()
+const expressPort = 3000
+
+
+// Websocket Setup
+var WebSocket = require('ws');
+const wsPort = 7071;
+const wss = new WebSocket.Server({ port: wsPort });
+
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/get_id', (req, res) => {
+    const id = uuidv4();
+    res.send({ id });
+})
+
+const expressServer = app.listen(expressPort, () => {
+    console.log(`Server running at http://localhost:${expressPort}`);
+    console.log(`Websocket Server running at ws://localhost:${wsPort}/ws\n\n`);
+});
+
+expressServer.on("upgrade", (httpRequest, socket, head) => {
+    wss.handleUpgrade(httpRequest, socket, head, (websocket) => {
+        wss.emit("connection", websocket, httpRequest);
+    });
+});
+
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+
+
+const clients = new Map();
+wss.on('connection', (ws, httpRequest) => {
+    // const queryObject = url.parse(req.url, true).query;
+    // console.log('Query Parameters:', queryObject);
+
+    // if (queryObject.id) {
+    //     console.log('No id present!')
+    //     ws.send(JSON.stringify({ error: 'No id provided' }));
+    //     ws.close();
+    //     return;
+    // }
+
+    // const id = queryObject.id;
+    const id = uuid.v4();
+    const color = Math.floor(Math.random() * 360);
+    const metadata = { id, color };
+
+    clients.set(ws, metadata);
+
+    ws.on("message", (messageStr) => {
+        const message = JSON.parse(messageStr);
+        console.log(message);
+        const metadata = clients.get(ws);
+
+        message.sender = metadata.id;
+        message.color = metadata.color;
+        const outbound = JSON.stringify(message);
+
+        [...clients.keys()].forEach((client) => {
+            client.send(outbound);
+        });
+    });
+
+    ws.on("close", () => {
+        clients.delete(ws);
+    });
+}
+);
